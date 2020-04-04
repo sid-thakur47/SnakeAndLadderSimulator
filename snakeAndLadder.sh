6dc72835bc64bc2565e9bced49ea4f940db0e3f3
sid-thakur47 <sidthakur258@gmail.com>
2020-04-04 15:09:29 +0530

    ADDED:Function to repeat the game till player wins

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,10 +2,24 @@
 
 playerOnePosition=0
 
-NO_PLAY	=1
+NO_PLAY=1
 LADDER=2
 SNAKE=3
 START_POSITION=0
+WIN_POSITION=100
+
+function continueTillPlayerWins() {
+	for (( ;; ))
+	do
+		echo "Current position of player: $playerOnePosition"
+		if [ $playerOnePosition -gt $WIN_POSITION ]
+		then
+			break
+		else
+			checkOption
+		fi
+	done
+}
 
 function dieRoll() {
 	number=$((RANDOM%6+1))
@@ -15,6 +29,14 @@ function dieRoll() {
 function getOption() {
 	option=$(($((RANDOM%3))+1))
 	echo $option
+}
+
+function restartGame() {
+	if [ $playerOnePosition -lt $START_POSITION ]
+	then
+		echo "You moved to position 0 player restarts from 0"
+		playerOnePosition=$START_POSITION
+	fi
 }
 
 function checkOption() {
@@ -34,7 +56,9 @@ function checkOption() {
 		$SNAKE)
 			playerOnePosition=$(($playerOnePosition-$roll))
 			echo "You Rolled dice of $roll position"
+			restartGame
 			echo -e "you are unlucky eaten by snake\nPlayer moved down up to position: $playerOnePosition\n"
 			;;
 	esac
 }
+continueTillPlayerWins
