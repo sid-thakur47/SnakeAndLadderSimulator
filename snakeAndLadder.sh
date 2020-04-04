24005ab1ed2f20f496f9b9a32185afa48977a1fd
sid-thakur47 <sidthakur258@gmail.com>
2020-04-04 14:41:16 +0530

    REFACTORED:Changed Indentation

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -22,19 +22,19 @@ function checkOption() {
 	roll="$(dieRoll)"
 	case $selectOption in
 			$NO_PLAY)
-					echo -e "player doesnt play\n"
-					;;
+				echo -e "player doesnt play\n"
+				;;
 
 			$LADDER)
-					playerOnePosition=$(($playerOnePosition+$roll))
-					echo "You Rolled dice of $roll position"
-					echo -e "You are lucky got a ladder\nPlayer moved up to position: $playerOnePosition\n"
-					;;
+				playerOnePosition=$(($playerOnePosition+$roll))
+				echo "You Rolled dice of $roll position"
+				echo -e "You are lucky got a ladder\nPlayer moved up to position: $playerOnePosition\n"
+				;;
 
 			$SNAKE)
-					playerOnePosition=$(($playerOnePosition-$roll))
-					echo "You Rolled dice of $roll position"
-					echo -e "you are unlucky eaten by snake\nPlayer moved down up to position: $playerOnePosition\n"
-					;;
+				playerOnePosition=$(($playerOnePosition-$roll))
+				echo "You Rolled dice of $roll position"
+				echo -e "you are unlucky eaten by snake\nPlayer moved down up to position: $playerOnePosition\n"
+				;;
 	esac
 }
